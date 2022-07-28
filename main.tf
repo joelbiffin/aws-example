2a6186525def6882a7ecb1e9cd84757d2ad5bdf3
Joel Biffin <joel@theroom.io>
2022-07-28 19:05:20 +0100

    Create hello world lambda function with required dependencies
    Dependencies:
      - CloudwatchEventRule (publish events on a schedule)
      - CloudwatchEventTarget (specify scheduler event destination - lambda)
      - Lamdba function
      - Lambda permission (allowing cloudwatch to send events to Lambda)
      - IAM role (permission for lambda to be spun up)
    
    In order to manage AWS with Terraform, we created an IAM user in AWS
    using the web console UI - this user has "AdministrativeAccess"
    permissions which allows it to make changes to AWS infrastructure.
    
    Input variables:
      - Access Key (belongs to Terraform IAM user)
      - Secret Key (belongs to Terraform IAM user)
      - AWS Region (points to eu-west2 - London)
      - Key path (points to my local OpenSSH certificate which was generated
        in the EC2 > KeyPairs UI - this allows pushing up code to AWS from
        my machine)

---
+++ main.tf
@@ -0,0 +1,72 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+
+provider "aws" {
+    access_key = var.access_key
+    region     = var.aws_region
+    secret_key = var.secret_key
+}
+
+
+data "aws_iam_policy_document" "lambda_assume_role_policy" {
+    statement {
+        actions = ["sts.AssumeRole"]
+        effect = "Allow"
+
+        principals {
+            identifiers = ["lambda.amazonaws.com"]
+            type = "Service"
+        }
+    }
+}
+
+resource "aws_iam_role" "lambda_role" {
+    assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+    name               = "lambda-lambdaRole-waf"
+}
+
+
+resource "aws_cloudwatch_event_rule" "every_minute" {
+    description         = "Triggers every minute"
+    name                = "every_minute"
+    schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "trigger_hello_world_every_minute" {
+    arn       = aws_lambda_function.lambda.arn
+    rule      = aws_cloudwatch_event_rule.every_minute.name
+    target_id = "hello_world"
+}
+
+resource "aws_lambda_permission" "allow_cloudwatch_to_trigger_hello_word" {
+    action        = "lamdba:InvokeFunction"
+    function_name = aws_lambda_function.hello_world.function_name
+    principal     = "events.amazonaws.com"
+    source_arn    = aws_cloudwatch_event_rule.every_minute.arn
+    statement_id  = "AllowExecutionFromCloudwatch"
+}
+
+
+data "archive_file" "hello_world_function" {
+    output_path = "hello_world_lambda.zip"
+    source      = "lambda/hello_world/main.py"
+    type        = "zip"
+}
+
+resource "aws_lambda_function" "hello_world" {
+    filename         = "hello_world.zip"
+    function_name    = "hello_world"
+    handler          = "main.handler"
+    role             = aws_iam_role.lambda_role.arn
+    runtime          = "python3.9"
+    source_code_hash = data.archive_file.hello_world_function.output_base64sha256
+    timeout          = 10
+}
+
